77813e2e7e85525163bcd5933e4d6d68e8494f9f
elcio <elcioferreira398@gmail.com>
2026-06-11 18:19:36 +0000

    atividades Aulas 2 e 3

---
+++ logica_prog/Aulas/Aula_2/aula_2.js
@@ -0,0 +1,10 @@
+//Criar variáveis para representar uma situação do cotidiano.
+
+const NOME_MERCADO = "Mercadinho do ibura";
+
+let produto = "Arroz";
+let preco = 25.50;
+let quantidade = 2;
+let total = preco * quantidade;
+
+console.log("Produto:",produto,"        Total a pagar:",total);
