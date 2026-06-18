bdf7afd53c8fdd7fa9756550e1f59506b88c300e
elcio <elcioferreira398@gmail.com>
2026-06-18 17:05:17 +0000

    aula 8 e 9

--- logica_prog/Aulas/Aula_8/atividade2.js
+++ logica_prog/Aulas/Aula_8/atividade2.js
@@ -10,13 +10,30 @@ rl.question("Digite o valor da compra: ", function(resposta) {
   let desconto = 0;
   let valorFinal = 0;
 
+
+
+
   if (valorCompra < 0) {
     console.log("Valor inválido.");
+
+
+
+
+
+
+
   } else if (valorCompra <= 50) {
     desconto = 0;
     valorFinal = valorCompra - desconto;
     console.log("Valor original: R$ " + valorCompra);
     console.log("Valor com desconto: R$ " + valorFinal);
+
+
+
+
+
+
+
   } else if (valorCompra >= 51 && valorCompra <= 100) {
     desconto = valorCompra * 0.10;
     valorFinal = valorCompra - desconto;
---
+++ logica_prog/Aulas/Aula_9/aula_9.js
@@ -0,0 +1,14 @@
+// repetição e Função
+
+// Criar dois algoritmos em linguagem natural, sem código, para situações do cotidiano que envolvam repetição.
+
+// Fazer séries de exercício na academira
+// Escolher o exercício que vai fazer
+// Definir quantas séries
+// Preparar o equipamento necessário
+// Fazer a primeira série do exercício
+// Descansar
+// Verificar quantas séries faltam para completar
+// Repetir o ciclo de fazer a série
+// Repetir o ciclo de descansar até completar todas
+// Quando todas as séries forem concluídas, encerrar o exercício
